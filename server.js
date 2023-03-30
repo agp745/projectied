ef82366165e4fbca72e22ed156fe174e6abd21c2
root <root@DESKTOP-TCD1NHF>
2023-03-30 16:25:59 -0500

    first day fixed

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mustacheExpress = require('mustache-express')
 const session = require('express-session')
 const path = require('path')
 const VIEWS_PATH = path.join(__dirname, '/views')
-app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials','.mustache'))
+const PARTIALS_PATH = path.join(VIEWS_PATH, '/partials')
+app.engine('mustache', mustacheExpress(PARTIALS_PATH,'.mustache'))
 app.set('views', VIEWS_PATH)
 app.set('view engine', 'mustache')
 app.use(express.urlencoded())
@@ -14,7 +15,7 @@ const PORT = process.env.PORT
 const authRouter = require("./routes/auth")
 const appRouter = require("./routes/app")
 
-app.engine("mustache", mustacheExpress())
+// app.engine("mustache", mustacheExpress())
 
 app.set("views", "./views")
 app.set("view engine", "mustache")
@@ -33,6 +34,7 @@ app.get("/", (req, res) => {
     res.redirect("/auth/login")
 })
 
+console.log(PARTIALS_PATH)
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
