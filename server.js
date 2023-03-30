0a1e47a139f97a6c28dba205e06fe05dfd2d067d
Alex Perez <alejandro.gperez745@gmail.com>
2023-03-30 16:31:28 -0500

    removed duplicate MWare in Server.js

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require("express")
 const app = express()
-const mustacheExpress = require('mustache-express')
-const session = require('express-session')
-const path = require('path')
-const VIEWS_PATH = path.join(__dirname, '/views')
-const PARTIALS_PATH = path.join(VIEWS_PATH, '/partials')
-app.engine('mustache', mustacheExpress(PARTIALS_PATH,'.mustache'))
-app.set('views', VIEWS_PATH)
-app.set('view engine', 'mustache')
-app.use(express.urlencoded())
-require('dotenv').config()
-app.use(express.static('static'))
+const mustacheExpress = require("mustache-express")
+const session = require("express-session")
+const path = require("path")
+const VIEWS_PATH = path.join(__dirname, "/views")
+const PARTIALS_PATH = path.join(VIEWS_PATH, "/partials")
+require("dotenv").config()
+
+app.engine("mustache", mustacheExpress(PARTIALS_PATH, ".mustache"))
+
+app.set("views", VIEWS_PATH)
+app.set("view engine", "mustache")
+
 const PORT = process.env.PORT
 const authRouter = require("./routes/auth")
 const appRouter = require("./routes/app")
 
-// app.engine("mustache", mustacheExpress())
-
-app.set("views", "./views")
-app.set("view engine", "mustache")
-
 app.use(express.urlencoded())
+app.use(express.static("static"))
 app.use(
     session({
         secret: process.env.SESSION_SECRET_KEY,
@@ -34,7 +31,6 @@ app.get("/", (req, res) => {
     res.redirect("/auth/login")
 })
 
-console.log(PARTIALS_PATH)
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
