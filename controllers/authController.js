85fe4d3e92e7fac00e7c70e8336a3698077d059d
Alex Perez <alejandro.gperez745@gmail.com>
2023-04-05 11:06:21 -0500

    added pfp to users table

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,7 @@ const getUser = async (req, res) => {
     console.log(req.oidc.user)
     const username = req.oidc.user.nickname
     const email = req.oidc.user.email
+    const picture = req.oidc.user.picture
 
     const user = await models.User.findOne({
         where: {
@@ -21,12 +22,24 @@ const getUser = async (req, res) => {
         },
     })
 
+    if (user.dataValues.picture === null) {
+        await models.User.update(
+            { picture: picture },
+            {
+                where: {
+                    id: user.dataValues.id,
+                },
+            }
+        )
+    }
+
     if (user) {
         res.redirect("/home")
     } else {
         const newUser = await models.User.build({
             username: username,
             email: email,
+            picture: picture,
         })
 
         await newUser.save()
