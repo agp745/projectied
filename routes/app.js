253e0642ced3886a50723acf1c9003fba69a9227
Alex Perez <alejandro.gperez745@gmail.com>
2023-04-05 17:09:16 -0500

    completed todo list api

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const appController = require("../controllers/appController")
+const projectController = require("../controllers/projectController")
 
 router.get("/home", appController.listProjects)
 
@@ -10,10 +11,21 @@ router.post("/create-project", appController.createProject)
 
 router.get("/project/:project_id", appController.renderProject)
 
-router.post("/project/todo/:project_id")
+//----------------------------------------------------
 
-router.post("/project/active/:project_id")
+router.get("/project/todo/:project_id", projectController.sendTodo)
 
-router.post("/project/completed/:project_id")
+router.post("/project/todo/:project_id", projectController.addTodo)
+
+router.get("/project/active/:project_id", projectController.sendActives)
+
+router.post("/project/active/:project_id/:todo_id", projectController.activate)
+
+router.get("/project/completed/:project_id", projectController.sendCompleted)
+
+router.post(
+    "/project/completed/:project_id/:active_id",
+    projectController.complete
+)
 
 module.exports = router
